1a597e8e8442a948abf48ba97089273c04a78407
Harrison Ryan <hwryan12@gmail.com>
2023-06-29 13:33:12 -0500

    feature: Finalize Turbo Streams for CRUD

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,5 +2,6 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
-  after_create_commit -> { broadcast_prepend_to "quotes" }
+
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
